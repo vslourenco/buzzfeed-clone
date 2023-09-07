54e80d433ed537c33b5de54f23d37f0c1ae1cf09
Vinicius S. Lourenco <vinicius.vsl@gmail.com>
2023-09-07 17:36:34 -0300

    Project started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { QuizComponent } from './components/quiz/quiz.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuizComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
